bc4d2dcdeac5ec919e4cc32c4e1e7804bd722887
ronchabarr <ronchabarr@gmail.com>
2020-11-21 11:39:53 +0200

    add general files
    hi or!

---
+++ Assets/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{   public Text Kmh;
+    public GameObject Track;
+   public  GameObject[] Routs;
+    public GameObject player;
+    public int playerCourentRout;
+    internal bool RightborderBlock=false;
+    internal bool LeftborderBlock=false;
+    internal int Streake;
+    public string[] colors = new string[] {"Red", "Yellow", "Green" };
+    internal string currentTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int ChildCount = Track.transform.childCount;
+        Routs = new GameObject[ChildCount];
+        for (int i = 0; i < ChildCount; i++)
+        {
+            Routs[i] = Track.transform.GetChild(i).gameObject;
+        }
+        playerCourentRout = ((ChildCount-1) + 1) / 2;
+        StartCoroutine(ColorSwich());
+
+
+
+     
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        player.transform.position = new Vector3(Routs[playerCourentRout].transform.position.x, player.transform.position.y, player.transform.position.z);
+        if (playerCourentRout == Track.transform.childCount-1)
+        {
+            RightborderBlock = true;
+        }else
+            RightborderBlock = false;
+
+        if (playerCourentRout == Track.transform.childCount - Track.transform.childCount)
+        {
+            LeftborderBlock = true;
+
+        }
+        else
+            LeftborderBlock = false;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)&&!LeftborderBlock)
+        {
+            playerCourentRout--;
+            
+        } 
+        if (Input.GetKeyDown(KeyCode.RightArrow)&&!RightborderBlock)
+        {
+            playerCourentRout++;
+            
+        }
+
+    }
+
+    IEnumerator ColorSwich()
+    {
+        currentTarget = colors[Random.Range(0,3)];
+        
+        yield return new WaitForSeconds(3);
+        StartCoroutine(ColorSwich());
+        
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{ public GameObject gameManager;
+    Rigidbody rb;
+    Vector3 Movement = new Vector3();
+    public int speed;
+     int myStreak;
+    internal string myTarget;
+    private Text mykmh;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Movement = Vector3.forward;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTarget = gameManager.GetComponent<GameManager>().currentTarget;
+        if (myTarget == "Red")
+        {
+
+            this.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        } 
+        if (myTarget == "Yellow")
+        {
+
+            this.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+        } 
+        if (myTarget == "Green")
+        {
+
+            this.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log(rb.velocity.z);
+            Debug.Log(myTarget);
+        }
+        if (myStreak > 0)
+        {
+
+        rb.velocity = Movement*(speed*myStreak);
+        }
+        else
+        rb.velocity = Movement*(speed);
+
+        mykmh.text = myStreak.ToString();
+        gameManager.GetComponent<GameManager>().Kmh.text = mykmh.text;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(myTarget))
+        {
+            myStreak++;
+            Notify();
+            Debug.Log("RightColor");
+
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+        else
+        {
+            myStreak --;
+            Debug.Log("WrongColor");
+        }
+      
+        
+    }
+
+
+
+
+
+
+    public void Notify()
+    {
+
+        gameManager.GetComponent<GameManager>().Streake = myStreak;
+    }
+
+    
+}
